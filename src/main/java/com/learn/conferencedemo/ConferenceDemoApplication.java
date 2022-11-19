0475d732a4d462546c2830ffd8c3637c511604b3
Pratyush Sinha <2021sp93070@wilp.bits-pilani.ac.in>
2022-11-19 10:44:38 +0530

    hello world

--- src/main/java/com/learn/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/learn/conferencedemo/ConferenceDemoApplication.java
@@ -8,7 +8,6 @@ public class ConferenceDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceDemoApplication.class, args);
-
-
+		System.out.println("Hello world");
 	}
 }
