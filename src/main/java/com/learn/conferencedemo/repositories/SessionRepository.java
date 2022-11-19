c441da64618a2ec1b00745618024ce21f20e0771
Pratyush Sinha <2021sp93070@wilp.bits-pilani.ac.in>
2022-11-19 10:58:56 +0530

    adde, delted, modified a file

--- src/main/java/com/learn/conferencedemo/repositories/SessionRepository.java
+++ src/main/java/com/learn/conferencedemo/repositories/SessionRepository.java
@@ -4,4 +4,5 @@ import com.learn.conferencedemo.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
+	
 }
